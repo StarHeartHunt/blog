3a09564441feafc159a092ba6f8ea9f9d0e288f8
StarHeartHunt <starheart233@gmail.com>
2023-04-14 09:25:25 +0800

    fix: typing

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,13 @@
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
-import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypePrettyCode from 'rehype-pretty-code';
 import { remarkReadingTime } from './src/utils/frontmatter';
-
-import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
@@ -41,7 +40,7 @@ export default defineConfig({
           keepBackground: true,
           // Callback hooks to add custom logic to nodes when visiting
           // them.
-          onVisitLine(node) {
+          onVisitLine(node: { children: string | unknown[] }) {
             // Prevent lines from collapsing in `display: grid` mode, and
             // allow empty lines to be copy/pasted
             if (node.children.length === 0) {
@@ -53,11 +52,11 @@ export default defineConfig({
               ];
             }
           },
-          onVisitHighlightedLine(node) {
+          onVisitHighlightedLine(node: { properties: { className: string[] } }) {
             // Each line node by default has `class="line"`.
             node.properties.className.push('highlighted');
           },
-          onVisitHighlightedWord(node) {
+          onVisitHighlightedWord(node: { properties: { className: string[] } }) {
             // Each word node has no className by default.
             node.properties.className = ['word'];
           },
--- src/utils/frontmatter.ts
+++ src/utils/frontmatter.ts
@@ -1,10 +1,9 @@
-import getReadingTime from 'reading-time';
+import type { RemarkPlugin } from '@astrojs/markdown-remark';
 import { toString } from 'mdast-util-to-string';
+import getReadingTime from 'reading-time';
 
-export function remarkReadingTime() {
-  return function (tree, { data }) {
-    const textOnPage = toString(tree);
-    const readingTime = getReadingTime(textOnPage);
-    data.astro.frontmatter.minutesRead = readingTime.text;
-  };
-}
+export const remarkReadingTime: RemarkPlugin = (tree, { data }) => {
+  const textOnPage = toString(tree);
+  const readingTime = getReadingTime(textOnPage);
+  data.astro.frontmatter.minutesRead = readingTime.text;
+};
