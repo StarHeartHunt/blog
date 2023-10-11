0dcc11e8379fb429b0ae4e3f95fec9be0af90d74
StarHeartHunt <starheart233@gmail.com>
2023-10-11 16:47:40 +0800

    chore: hide about page in blog list

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,7 @@ const blog = defineCollection({
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
+    hideInList: z.boolean().optional(),
   }),
 });
 
