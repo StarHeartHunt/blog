294decd05ed6269dd66e8f3581f3925b5eebcb4c
StarHeartHunt <starheart233@gmail.com>
2023-12-14 11:52:40 +0800

    revert: "chore: beian tweaks"
    This reverts commit 095a9b4cc0d4095f25b5e474044ee993766a6ba8.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "捕洛歌";
-export const SITE_DESCRIPTION = "StarHeart's 捕洛歌";
+export const SITE_TITLE = "StarHeart";
+export const SITE_DESCRIPTION = "StarHeart's Blog";
