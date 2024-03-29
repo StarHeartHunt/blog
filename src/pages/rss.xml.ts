8aa9576476441ed6e9a0126c6e2cc4746bdd99c2
StarHeartHunt <starheart233@gmail.com>
2024-03-29 16:00:01 +0800

    fix: add missing astro context type

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
 import { SITE_DESCRIPTION, SITE_TITLE } from "@/consts";
 import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
 import { getCollection } from "astro:content";
 
-export async function GET(context) {
+export async function GET(context: APIContext) {
   const posts = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
+    site: context.site!,
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.slug}/`,
