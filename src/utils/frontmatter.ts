760887fb501fdc8b449481275e477e83d870d831
StarHeart <starheart233@gmail.com>
2026-09-20 11:51:16 +0800

    chore(deps): update all dependencies (#105)

--- src/utils/frontmatter.ts
+++ src/utils/frontmatter.ts
@@ -5,9 +5,9 @@ import getReadingTime from "reading-time";
 export const remarkReadingTime: RemarkPlugin =
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (options = {}) =>
-  (tree, { data }) => {
-    const textOnPage = toString(tree);
-    const readingTime = getReadingTime(textOnPage);
-    if (data.astro?.frontmatter)
-      data.astro.frontmatter.minutesRead = readingTime.text;
-  };
+    (tree, { data }) => {
+      const textOnPage = toString(tree);
+      const readingTime = getReadingTime(textOnPage);
+      if (data.astro?.frontmatter)
+        data.astro.frontmatter.minutesRead = readingTime.text;
+    };
